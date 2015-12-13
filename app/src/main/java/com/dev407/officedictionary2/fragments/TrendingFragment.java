2640a18fe7b8530ef782f1da938e58de106e962a
administrator <administrator@loaner37-mbpro.local>
2015-12-13 12:04:48 -0800

    One more

--- app/src/main/java/com/dev407/officedictionary2/fragments/TrendingFragment.java
+++ app/src/main/java/com/dev407/officedictionary2/fragments/TrendingFragment.java
@@ -58,7 +58,7 @@ public class TrendingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_item_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_trending, container, false);
 
         // Set the adapter
         if (view instanceof RecyclerView) {
